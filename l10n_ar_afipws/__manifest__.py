4b4e9c72565a28773dd1aebb6c082db8bf2c4a7e
cguerrero73 <cristian.guerrero@gmail.com>
2026-09-15 13:03:40 +0000

    cambio a instalable y version en los manifest de las localizaciones

--- l10n_ar_afipws/__manifest__.py
+++ l10n_ar_afipws/__manifest__.py
@@ -1,8 +1,8 @@
 {
     "name": "Modulo Base para los Web Services de AFIP",
-    "version": "18.0.1.0.0",
+    "version": "1.0.1.0.0",
     "category": "Localization/Argentina",
-    "author": "ADHOC SA, Moldeo Interactive,Odoo Community Association (OCA)",
+    "author": "ADHOC SA, Moldeo Interactive,Odoo Community Association (OCA) by cguerrero73",
     "license": "AGPL-3",
     "summary": "",
     "depends": [
@@ -28,7 +28,7 @@
         "demo/parameter_demo.xml",
     ],
     "images": [],
-    "installable": False,
+    "installable": True,
     "auto_install": False,
     "application": False,
 }
--- l10n_ar_reports/__manifest__.py
+++ l10n_ar_reports/__manifest__.py
@@ -20,7 +20,7 @@
     ],
     "demo": [],
     "images": [],
-    "installable": False,
+    "installable": True,
     "auto_install": False,
     "application": False,
 }
